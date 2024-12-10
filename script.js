37a805a8d451fc81dec495b686b020e011c51dea
slysylveonn <sophgelly@gmail.com>
2024-12-10 14:57:17 -0500

    added subclass perishableproductproperties to script.js

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ class ProductProperties {
     }
 
 }
+class PerishableProductProperties extends ProductProperties {              //subclass of ProductProperties
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity);                    //calls parameters of parent class into subclass
+        this.expirationDate = expirationDate;            //additional parameter novel to subclass
+    }
+    toString() {
+        console.log(`Name: ${this.name} 
+        \nPrice: ${this.price} 
+        \nQuantity: ${this.quantity}
+        \nExpiration Date: ${this.expirationDate}`);
+    }
+};
